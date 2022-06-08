b45a3e313f9142b6ac5911aa951a99e35f9186b3
dmhliu <dliu@azaleasf.com>
2022-06-08 09:48:55 -0700

    Create item_get_shopify_tags.js

---
+++ item_get_shopify_tags.js
@@ -0,0 +1,12 @@
+    // Add additional code 
+...
+var headerObj = {
+    name: 'Accept-Language',
+    value: 'en-us'
+};
+var response = https.get({
+    url: 'https://www.testwebsite.com',
+    headers: headerObj
+});
+...
+// Add additional code 
