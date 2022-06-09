a8973b53d48c44058ac0e8bd77a52a2dc4a8e1ef
dmhliu <dliu@azaleasf.com>
2022-06-09 09:49:17 -0700

    Update item_get_shopify_tags.js

--- item_get_shopify_tags.js
+++ item_get_shopify_tags.js
@@ -10,10 +10,10 @@
 define(['N/record','N/https', 'N/search'],function(record,https,search){
   function get_product_tags(context){
     //  look at N/runtime.Script or script parameters  
-    // shopify app config page https://gumps-official.myshopify.com/admin/apps/private/295382778050
-    const user = '07dc4f4ce53608a7e7d086a94f8b8abc'  ; // this is not publicly visible in NS
-    const pass = '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';   //put into script params?
-    const host = 'gumps-official.myshopify.com';
+    // shopify app config page https://gumps-official.myshopify.com/admin/apps/private/
+    const user = '07dc4f4ce53608a7e7d086a94f8b8abc'  ; // this is not publicly visible in NS // dummy creds - live need to be stored in settings
+    //const pass = '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';   //put into script params?
+    //const host = 'gumps-official.myshopify.com';
   
   
     const apiURL=`https://${user}:${pass}@${host}/admin/api/unstable/orders/${shopifyOID}.json`;
